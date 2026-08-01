626d239ef5bf2380d6d6c5a9b86f0c90f07c9d20
JoeTheBest <jewelshahi10@gmail.com>
2026-08-02 02:46:26 +0300

    fix: update socket connection to use SOCKET_URL and include transports configuration

--- frontend/src/api/socket.js
+++ frontend/src/api/socket.js
@@ -2,8 +2,9 @@ import { io } from 'socket.io-client';
 import { SOCKET_URL } from '../constants/config';
 import { getClientId } from '../utils/clientId';
 
-const socket = io(SOCKET_URL, {
-  autoConnect: false, auth: { clientId: getClientId() }
+const socket = io(API_URL, {
+  transports: ['websocket'],
+  autoConnect: false,
 });
 
 export default socket;
